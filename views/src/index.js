a278b2250ab38582aa327283d07c12ab87b8d526
Diego Ezequiel <diego.scardulla@outlook.com>
2022-05-14 00:59:36 -0300

    detalle

--- views/src/index.js
+++ views/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import Navbar from "./components/Navbar.jsx";
 import Header from "./components/Header.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header />);
+root.render(<Navbar />, <Header />);
