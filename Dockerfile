a72488e61acd05ccbb211c3feec287c0e2b34e20
Diego Ezequiel <diego.scardulla@outlook.com>
2022-05-16 03:10:43 -0300

    creo que ahora si solucione todo xDDDDD

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+from node:18-alpine as base
+WORKDIR /src/
+COPY package*.json ./
+EXPOSE 3000
+
+FROM base as dev
+ENV NODE_ENV=dev
+RUN npm ci
+COPY --chown?node:node . ./
+USER node
+CMD ["npm", "run", "dev"]
--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,14 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
+			host: process.env.DB_HOST ||"localhost",
       database: process.env.POSTGRES_DB,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
     },
     migrations: {
       tableName: "knex_migrations",
-      directory: "./db/migrations/",
+      directory: "./api/db/migrations/",
     },
     seeds: {
       directory: "./api/db/seeds/",
