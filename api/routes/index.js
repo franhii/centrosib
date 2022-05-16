fd3933b463caa958da4f424d1d248f3aa89224c1
Diego Ezequiel <diego.scardulla@outlook.com>
2022-05-16 04:30:42 -0300

    super detallito

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,5 +4,8 @@ const doctorsRouter = require("./doctors");
 const router = express.Router();
 
 router.use("/api/v1/doctors", doctorsRouter);
+router.use("/", (req, res) => {
+	res.json({Estado: "Conectado papichurro"})
+});
 
 module.exports = router;
