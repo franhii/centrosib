67b602c2f5ac858361c8358edce82ec494436690
Diego Ezequiel <diego.scardulla@outlook.com>
2022-05-26 07:57:52 -0300

    con suerte no rompí nada

---
+++ views/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
+import NavLinks from "./NavLinks"
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+  return (
+    <div>
+      <div className="NavBar">
+        <nav>
+          {/* <button onClick={handleToggle}>
+            {navbarOpen ? (
+              <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+            ) : (
+              <FiMenu
+                style={{ color: "#7b7b7b", width: "40px", height: "40px" }}
+              />
+            )}
+          </button> */}
+          <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+            <li>
+              <NavLink to="/especialistas" onClick={() => closeMenu()}>
+                Especialistas
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <NavLinks />
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ views/src/Pages/index.js
@@ -0,0 +1,1 @@
+export { default as Especialistas } from './Especialistas/Especialistas.jsx';
