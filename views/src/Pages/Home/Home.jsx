7395e97654681466493620e8208a994897018b47
Emanuel Gonzalez <emagonz918@gmail.com>
2022-05-30 20:19:53 -0300

    aregue home

---
+++ views/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import banner from '../../Assets/img/banner.png'
+
+const Home
+    = () => {
+        return (
+            <div>
+                <div class="home__img">
+                    <img src={banner} alt="#" width="395px" />
+                </div>
+            </div>
+        )
+    }
+export default Home;
