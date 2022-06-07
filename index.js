77927db66937819891366b900ffb18011cf381f9
Francis Vaccarezza <francisvaccarezza@gmail.com>
2022-06-07 09:41:57 -0300

    updates de los pibes

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const dbSetup = require("./api/db/db-setup");
 const express = require("express");
 const router = require("./api/routes");
-
+const multer = require('multer');
 const PORT = process.env.PORT;
 
+
+//CONFIG DE LA DB(CREACION,ETC.)
 dbSetup();
+
+//CREATE EXPRESS APP
 const app = express();
 app.use(express.json());
 
+// MIDDLEWARES
+
+
+// ROUTES
 app.use("/", router);
 
+
+
+//SETTINGS
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
