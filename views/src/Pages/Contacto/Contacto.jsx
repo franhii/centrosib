696c2d062f03b44edadf625036f2bc6023811fa7
Andres Rampini <andres.f.rampini@gmail.com>
2022-06-08 20:10:56 -0300

    Agregue 2 cosas ñia

--- views/src/Pages/Contacto/Contacto.jsx
+++ views/src/Pages/Contacto/Contacto.jsx
@@ -3,7 +3,12 @@ import "./Contacto.css"
 
 const Contacto = () => {
   return (
-    <div>Contacto</div>
+    <div>
+      <div className="header-contact-details">
+						<span className="call-details"> <span className="tel-details">Tel.</span> <a href="tel:3462 518748" className="tel"> 3462 518748 </a> </span>
+						<span className="email-details"> <span className="mail-details">Email.</span> <a href="centrocib@gmail.com.ar" className="mail">centrosib@gmail.com.ar</a> </span>
+					</div>
+    </div>
   )
 }
 
