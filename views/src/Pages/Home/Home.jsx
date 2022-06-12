3be74568541069c6f0b402315cf8061226ddee6e
Diego Ezequiel <diego.scardulla@outlook.com>
2022-06-12 20:53:04 -0300

    Aviejo home

--- views/src/Pages/Home/Home.jsx
+++ views/src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import React from 'react'
 import './Home.css';
@@ -24,4 +25,34 @@ const Home
         </section>
         )
     }
+=======
+import React from 'react';
+import { banner, doctora, doctor } from '../../Assets/img';
+
+const Home = () => {
+  return (
+    <section class='home bd-grid' id='home'>
+      <img src={banner} />
+      <div class='home__data'>
+        <h1 class='home__title'>
+          ODONTOLOGICA <samp class='home__title-color'>SIB</samp>
+        </h1>
+        <a href='#contact' class='button'>
+          Contact
+        </a>
+      </div>
+      <div class='home__social'>
+        <a href='#' class='home__social-icon'>
+          <i class='bx bxl-instagram'></i>
+        </a>
+        <a href='#' class='home__social-icon'>
+          <i class='bx bxl-facebook'></i>
+        </a>
+        <a href='#' class='home__social-icon'></a>
+      </div>
+      <div class='home__img'></div>
+    </section>
+  );
+};
+>>>>>>> 3870ff0 (viejo home)
 export default Home;
