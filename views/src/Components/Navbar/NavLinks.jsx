95cf18ce1246218d748b99a42a99fb19c31fcb6f
Andres Rampini <andres.f.rampini@gmail.com>
2022-06-13 18:31:34 -0300

    Arreglado X2

--- views/src/Components/Navbar/NavLinks.jsx
+++ views/src/Components/Navbar/NavLinks.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Route, Routes } from "react-router-dom"
 import { Especialistas, Contacto, Nosotros } from '../../Pages'
 const PagesRoutes = () => {
@@ -20,20 +19,3 @@ const PagesRoutes = () => {
 	)
 }
 export default PagesRoutes
-=======
-import { Route, Routes } from "react-router-dom";
-import { Especialistas } from "../../Pages";
-import { Nosotros } from "../../Pages";
-const PagesRoutes = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={""} />
-        <Route path="/especialistas" element={<Especialistas />} />
-        <Route path="/nosotros" element={<Nosotros />} />
-      </Routes>
-    </div>
-  );
-};
-export default PagesRoutes;
->>>>>>> ea677ea4595e036c998212b968211efac9d3567e
