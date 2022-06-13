06ab32c8e1cc366fcdcb7c28fc8a5e6ce09b6b72
Andres Rampini <andres.f.rampini@gmail.com>
2022-06-13 18:30:53 -0300

    Arreglado

--- views/src/Pages/index.js
+++ views/src/Pages/index.js
@@ -1,10 +1,5 @@
 export { default as Especialistas } from './Especialistas/Especialistas.jsx';
-<<<<<<< HEAD
 export {default as Home } from './Home/Home.jsx';
 export {default as Nosotros } from './Nosotros/Nosotros.jsx';
 export {default as Skills } from './Skills/Skills.jsx';
 export {default as Contacto} from './Contacto/Contacto.jsx'
-=======
-export { default as Home } from './Home/Home.jsx';
-export { default as Nosotros } from './Nosotros/Nosotros.jsx';
->>>>>>> ea677ea4595e036c998212b968211efac9d3567e
